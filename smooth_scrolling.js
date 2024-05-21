ce5982697913c667753d74c44b3e07110bff498c
saket <jhasaket99dbg@gmail.com>
2024-05-21 17:59:14 +0530

    files added

---
+++ smooth_scrolling.js
@@ -0,0 +1,24 @@
+document.querySelectorAll("nav__link").forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = this.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+//if navbar has more element then use following code
+
+//add eventlistener to common parent element
+//determine which element originted the event
+document.querySelector("nav_link").addEventListener("click", function (e) {
+  e.preventDefault();
+  //Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+});
